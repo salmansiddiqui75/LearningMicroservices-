48fd811c737686cb522a87b76272fb283b9482a7
Salman siddiqui <salmansid8090@gmail.com>
2025-03-15 22:33:05 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ Configuration management/v1-springboot configuration/cards/src/main/java/com/sidtech/cards/CardsApplication.java
@@ -0,0 +1,41 @@
+package com.sidtech.cards;
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+/*@ComponentScans({ @ComponentScan("com.eazybytes.cards.controller") })
+@EnableJpaRepositories("com.eazybytes.cards.repository")
+@EntityScan("com.eazybytes.cards.model")*/
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Cards microservice REST API Documentation",
+				description = "EazyBank Cards microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Salman Siddiqu",
+						email = "salmansid8090@gmail.com"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://salmansiddiqui75.github.io/Portfolio/"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description =  "EazyBank Cards microservice REST API Documentation",
+				url = "https://salmansiddiqui75.github.io/Portfolio/"
+		)
+)
+public class CardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardsApplication.class, args);
+	}
+}
---
+++ Configuration management/v1-springboot configuration/cards/src/test/java/com/sidtech/cards/CardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sidtech.cards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
